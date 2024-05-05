527ef2e7c2e162f963e66f227ebc0244a57614fe
TobyNguyen710 <ductoby2003@gmail.com>
2024-05-05 15:00:27 -0600

    Move files forward 1 folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <>
-    
+      Portfolio
+      <About></About>
     </>
     // <>
     //   <div>
